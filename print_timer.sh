848dca707c75ba0b1243dabd52cf04c1ba95dee0
Jiansheng QIU <632863986@qq.com>
2020-12-20 15:38:48 +0800

    Update tools

--- print_timer.sh
+++ print_timer.sh
@@ -1,16 +1,16 @@
 set -e
 make
 sudo bash setup.sh
-cd /mnt/pmem
-#sudo fio -filename=./test1 -direct=1 -iodepth 1 -rw=write -ioengine=psync -bs=4K -thread -numjobs=1 -size=2G -name=randrw --dedupe_percentage=100 -group_reporting
-sudo fio -filename=./test1 -direct=1 -iodepth 1 -rw=write -ioengine=psync -bs=4K -thread -numjobs=1 -size=4K -name=randrw --dedupe_percentage=80 -group_reporting
-#sudo fio -filename=./test1 -direct=1 -iodepth 1 -rw=write -ioengine=psync -bs=4K -thread -numjobs=1 -size=2G -name=randrw --dedupe_percentage=50 -group_reporting
-#sudo fio -filename=./test1 -direct=1 -iodepth 1 -rw=write -ioengine=psync -bs=4K -thread -numjobs=1 -size=2G -name=randrw --dedupe_percentage=0 -group_reporting
-cd -
+sudo fio -filename=/mnt/pmem/test1 -direct=1 -iodepth 1 -rw=write -ioengine=sync -bs=4K -thread -numjobs=1 -size=3G -name=randrw --dedupe_percentage=0 -group_reporting
+
+sudo gcc ioctl_test.c -o ioctl_test && sudo ./ioctl_test
+sudo dmesg | tail -n 50
+
+time sudo rm /mnt/pmem/test1
+
 #sudo gcc ioctl_test.c -o ioctl_test && sudo ./ioctl_test
 #sudo dmesg | tail -n 50
-cd -
-time sudo rm test1
-cd -
-sudo umount /mnt/pmem
-sudo rmmod nova
+
+#sudo umount /mnt/pmem
+#sudo rmmod pmfs
+
