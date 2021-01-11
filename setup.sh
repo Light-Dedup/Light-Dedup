c8c6c49db94238afcba265434eefa0e32d3e6ae2
Jiansheng QIU <632863986@qq.com>
2021-01-11 16:09:04 +0800

    More setup output.

--- setup.sh
+++ setup.sh
@@ -2,10 +2,13 @@
 
 echo umounting...
 umount /mnt/pmem
+echo Removing the old kernel module...
 rmmod nova
+echo Inserting the new kernel module...
 insmod nova.ko measure_timing=0
 
 sleep 1
 
+echo mounting...
 #mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem
 mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
