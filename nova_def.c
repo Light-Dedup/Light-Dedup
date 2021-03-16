bfdb906d735b3aa5961a12b66d811fbd1da0fa3f
Jiansheng QIU <632863986@qq.com>
2021-03-16 14:25:20 +0800

    Rewrite memset_nt without asm, because the original asm assumes that length is 32 bits.

---
+++ nova_def.c
@@ -0,0 +1,25 @@
+#include <linux/kernel.h>
+#include <linux/module.h> // BUG_ON
+#include <linux/string.h>
+
+_Static_assert(sizeof(size_t) == 8, "sizeof(size_t) != 8!");
+/* assumes the length to be 4-byte aligned */
+void memset_nt(void *dest, uint32_t dword, size_t length)
+{
+	uint64_t qword = ((uint64_t)dword << 32) | dword;
+	BUG_ON(length % 4 != 0);
+	for (; length >= 64; length -= 64, dest = (char *)dest + 64) {
+		memcpy_flushcache((uint64_t *)dest, &qword, sizeof(uint64_t));
+		memcpy_flushcache((uint64_t *)dest + 1, &qword, sizeof(uint64_t));
+		memcpy_flushcache((uint64_t *)dest + 2, &qword, sizeof(uint64_t));
+		memcpy_flushcache((uint64_t *)dest + 3, &qword, sizeof(uint64_t));
+		memcpy_flushcache((uint64_t *)dest + 4, &qword, sizeof(uint64_t));
+		memcpy_flushcache((uint64_t *)dest + 5, &qword, sizeof(uint64_t));
+		memcpy_flushcache((uint64_t *)dest + 6, &qword, sizeof(uint64_t));
+		memcpy_flushcache((uint64_t *)dest + 7, &qword, sizeof(uint64_t));
+	}
+	for (; length >= 8; length -= 8, dest = (char *)dest + 8)
+		memcpy_flushcache((uint64_t *)dest, &qword, sizeof(uint64_t));
+	if (length == 4)
+		memcpy_flushcache((uint32_t *)dest, &dword, sizeof(uint32_t));
+}
