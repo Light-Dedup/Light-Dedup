46355f09ae615a52788f569c051eb3716d5e339a
Jiansheng QIU <632863986@qq.com>
2021-03-20 01:04:40 +0800

    data_cow by default

--- setup.sh
+++ setup.sh
@@ -10,5 +10,6 @@ insmod nova.ko measure_timing=0
 sleep 1
 
 echo mounting...
-#mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem
-mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
+mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem
+#mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
+#mount -t NOVA -o init /dev/pmem0 /mnt/pmem
