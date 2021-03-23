2c7faca211f6be845982c5bf5ffeb0b4e790f16b
Jiansheng QIU <632863986@qq.com>
2021-03-23 12:16:06 +0800

    remount.sh default data_cow

--- remount.sh
+++ remount.sh
@@ -4,6 +4,6 @@ echo umounting...
 time umount /mnt/pmem
 
 echo remounting...
-#mount -t NOVA -o wprotect,data_cow /dev/pmem0 /mnt/pmem
-time mount -t NOVA -o wprotect /dev/pmem0 /mnt/pmem
+time mount -t NOVA -o wprotect,data_cow /dev/pmem0 /mnt/pmem
+#mount -t NOVA -o wprotect /dev/pmem0 /mnt/pmem
 # ./ioctl_test
