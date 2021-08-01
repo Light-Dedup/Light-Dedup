008b8c3e79478ea82f81c02c3f1c579f97622047
Jiansheng QIU <632863986@qq.com>
2021-08-01 19:37:54 +0800

    Fix nova_queue_expand(but why?)

--- queue.c
+++ queue.c
@@ -18,35 +18,27 @@ static int
 nova_queue_expand(struct kfifo *q)
 {
 	struct kfifo tmp;
-	size_t len = kfifo_len(q);
-	size_t left = kfifo_size(q);
-	char *buf = vmalloc(len << 1);
-	char *buf2;
+	size_t size = kfifo_size(q);
+	size_t left = kfifo_len(q);
+	char *buf = vmalloc(size << 1);
+	unsigned long buf2;
 	size_t copied;
 	int ret;
 	if (buf == NULL) {
 		ret = -ENOMEM;
 		goto err_out0;
 	}
-	buf2 = vmalloc(len);
-	if (buf2 == NULL) {
-		ret = -ENOMEM;
-		goto err_out1;
-	}
-	ret = kfifo_init(&tmp, buf, len << 1);
+	ret = kfifo_init(&tmp, buf, size << 1);
 	if (ret)
-		goto err_out2;
+		goto err_out1;
 	while (left) {
-		copied = kfifo_out(q, buf2, left);
-		BUG_ON(kfifo_in(&tmp, buf, copied) != copied);
+		copied = kfifo_out(q, &buf2, sizeof(buf2));
+		BUG_ON(kfifo_in(&tmp, &buf2, sizeof(buf2)) != copied);
 		left -= copied;
 	}
-	vfree(buf2);
 	vfree(q->kfifo.data);
 	*q = tmp;
 	return 0;
-err_out2:
-	vfree(buf2);
 err_out1:
 	vfree(buf);
 err_out0:
@@ -57,6 +49,7 @@ int nova_queue_push(struct nova_queue *_q, void *data, size_t len)
 	struct kfifo *q = &_q->q;
 	size_t copied = kfifo_in(q, data, len);
 	int ret;
+	// printk("%s: %lu\n", __func__, *(unsigned long *)data);
 	if (copied == len) {
 		return 0;
 	}
