23667d580685ae2a5b4be97e3bf1fb114259f518
Jiansheng QIU <632863986@qq.com>
2022-03-26 02:10:14 -0400

    Add some essential files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+echo umounting...
 umount /mnt/pmem
+echo Removing the old kernel module...
 rmmod nova
+echo Inserting the new kernel module...
 insmod nova.ko measure_timing=0
 
 sleep 1
 
-mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
+echo mounting...
+mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem
+#mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
+#mount -t NOVA -o init /dev/pmem0 /mnt/pmem
