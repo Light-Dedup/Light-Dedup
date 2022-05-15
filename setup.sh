b7140b2f30143fe9e4dcf4f2a7c19aed493e5c68
Jiansheng QIU <632863986@qq.com>
2022-05-15 02:07:47 -0400

    setup.sh: make -j$(nproc)

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+if [ ! $1 ]; then
+    timing=0
+else
+    timing=$1
+fi
+make -j$(nproc)
 echo umounting...
-umount /mnt/pmem
+sudo umount /mnt/pmem
 echo Removing the old kernel module...
-rmmod nova
+sudo rmmod nova
 echo Inserting the new kernel module...
-insmod nova.ko measure_timing=0
+sudo insmod nova.ko measure_timing=$timing
 
 sleep 1
 
 echo mounting...
-mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem
-#mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
-#mount -t NOVA -o init /dev/pmem0 /mnt/pmem
+sudo mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem
+#sudo mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
+#sudo mount -t NOVA -o init /dev/pmem0 /mnt/pmem
