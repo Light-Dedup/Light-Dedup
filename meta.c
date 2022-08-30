5339c568ff37ec35df672de5af84ea94bbc93432
deadpool <2841954631@qq.com>
2022-08-30 21:37:58 +0800

    remove some prints

--- meta.c
+++ meta.c
@@ -178,9 +178,9 @@ static int table_decrer_execute_global(struct nova_meta_table *table, bool nvm) 
 
 	for (i = 0; i < out_num; i++) {
 		item = &items[i];
-		if (nvm) {
-			nova_dbg("%s: retrieve from nvm %ld", __func__, item->blocknr);		
-		}
+		// if (nvm) {
+		// 	nova_dbg("%s: retrieve from nvm %ld", __func__, item->blocknr);		
+		// }
 		nova_meta_table_decr(table, item->blocknr);
 	}
 
