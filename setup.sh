fe6406ccf2e5bceb933f92c9b301cd36981832f1
Jiansheng QIU <632863986@qq.com>
2022-09-10 16:30:49 +0800

    Revert setup.sh

--- setup.sh
+++ setup.sh
@@ -5,19 +5,17 @@ if [ ! $1 ]; then
 else
     timing=$1
 fi
-
-sleep 5
-
+make -j$(nproc)
 echo umounting...
-umount /mnt/pmem0
+sudo umount /mnt/pmem
 echo Removing the old kernel module...
-rmmod nova
+sudo rmmod nova
 echo Inserting the new kernel module...
-insmod nova.ko measure_timing=$timing
+sudo insmod nova.ko measure_timing=$timing
 
 sleep 1
 
 echo mounting...
-mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem0
-#mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem0
-#mount -t NOVA -o init /dev/pmem0 /mnt/pmem0
+sudo mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem
+#sudo mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
+#sudo mount -t NOVA -o init /dev/pmem0 /mnt/pmem
